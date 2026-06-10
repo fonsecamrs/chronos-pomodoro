79c47bfcd02868a68b2d4d4ca032537158f3233e
Marcos Fonseca <marcos.fonseca1983@gmail.com>
2026-06-10 18:31:07 -0300

    Estrutura do projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,57 @@
 import './styles/theme.css';
 import './styles/global.css';
 
-import { Heading } from './components/Heading';
+//import { TimerIcon } from 'lucide-react';
+//import { Heading } from './components/Heading';
 
 export function App() {
   return (
     <>
-      <Heading />
+      <div className='container-fluid'>
+        <div className='container'>
+          <div className='content'>
+            <section>ESTRUTURA DO PROJETO</section>
+          </div>
+        </div>
+      </div>
+
+      <div className='container'>
+        <div className='content'>
+          <section>LOGO</section>
+        </div>
+      </div>
+
+      <div className='container'>
+        <div className='content'>
+          <section>MENU</section>
+        </div>
+      </div>
+
+      <div className='container'>
+        <div className='content'>
+          <section>FORM</section>
+        </div>
+      </div>
+
+      <div className='container'>
+        <div className='content'>
+          <section>FOOTER</section>
+        </div>
+      </div>
+    </>
+  );
+  /* return (
+    <>
+      <Heading>INICIANDO A ESTRUTURA DO PROJETO</Heading>
+      <br />
+      <Heading>
+        CABEÇALHO TESTE
+        <button>
+          <TimerIcon />
+        </button>
+      </Heading>
       <br />
       <p>Olá mundo! (Importando do componente App)</p>
     </>
-  );
+  ); */
 }
